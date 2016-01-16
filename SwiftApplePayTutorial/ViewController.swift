f0484415530de6432bdb46b034c6c369828617a8
Dylan McKee <dylan@djmckee.co.uk>
2016-01-16 22:23:09 +0000

    Initial Commit

---
+++ SwiftApplePayTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApplePayTutorial
+//
+//  Created by Dylan McKee on 16/01/2016.
+//  Copyright © 2016 moltin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
