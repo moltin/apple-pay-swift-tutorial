90d4e1da8aa1ec802db35f7b2e90614dabef26dc
Dylan McKee <dylan@djmckee.co.uk>
2016-01-19 17:46:33 +0000

    Tutorial code

---
+++ BridgingHeader.h
@@ -0,0 +1,16 @@
+//
+//  BridgingHeader.h
+//  SwiftApplePayTutorial
+//
+//  Created by Dylan McKee on 17/01/2016.
+//  Copyright © 2016 moltin. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <Moltin/Moltin.h>
+#import <Stripe/Stripe.h>
+#import <Stripe/Stripe+ApplePay.h>
+
+#endif /* BridgingHeader_h */
